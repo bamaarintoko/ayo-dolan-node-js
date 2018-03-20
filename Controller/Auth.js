a3160303fb4f009d63ad04541e70321bfc9f6ab2
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-03-20 17:26:19 +0700

    fix auth login

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -6,31 +6,28 @@ let upload = multer();
 const app = express();
 const login = require('../Model/Login');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(bodyParser.urlencoded({extended: false})); // support encoded bodies
 app.use(bodyParser.json()); // support json encoded bodies
 
-app.post('/login', upload.fields([]), (req, res)=>{
-    let user_name       = req.body.username
-    let password        = md5(md5(req.body.password).toString()).toString()
-    const params        = {user_name,password}
-
-    if (user_name==="" || password===""){
-    res.json("kosong")
-
+app.post('/login', upload.fields([]), (req, res) => {
+    let user_name = req.body.username
+    let password = md5(md5(req.body.password).toString()).toString()
+    const params = {user_name, password}
+    if (req.body.username < 1 || req.body.password.length < 1) {
+        let val = [];
+        val.push({status: false, message: "email and password required", results: []})
+        res.json(val[0])
     } else {
-        res.json(params)
-        login.login(params).then((response)=>{
+        login.login(params).then((response) => {
             let val = [];
-            if (response.length <1){
-                val.push({status:false,text:"email or password wrong",data_user:[]})
+            if (response.length < 1) {
+                val.push({status: false, message: "email or password wrong", results: []})
             } else {
-                val.push({status:true,text:"login success",data_user:response})
+                val.push({status: true, message: "login success", results: response})
             }
             res.json(val[0])
         })
     }
-
-
 })
 
 module.exports = app
